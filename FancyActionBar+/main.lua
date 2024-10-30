729da7310414c2ce88eea87c239f309de4e68da4
Anthony S. Castanza <acastanza@gmail.com>
2024-10-29 22:13:17 -0700

    Doc and Version

--- FancyActionBar+/main.lua
+++ FancyActionBar+/main.lua
@@ -4,7 +4,7 @@ local FancyActionBar = FancyActionBar;
 -----------------------------[    Constants   ]--------------------------------
 -------------------------------------------------------------------------------
 local NAME = "FancyActionBar+";
-local VERSION = "2.9.2";
+local VERSION = "2.9.3";
 local slashCommand = "/fab" or "/FAB";
 local EM = GetEventManager();
 local WM = GetWindowManager();
