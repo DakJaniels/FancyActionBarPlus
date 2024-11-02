0f66c26291eed97cd3b2f3cb172fe1de62b3a317
Anthony S. Castanza <acastanza@gmail.com>
2024-11-01 19:49:14 -0700

    Update Doc for Release

--- FancyActionBar+/menu.lua
+++ FancyActionBar+/menu.lua
@@ -4672,7 +4672,7 @@ function FancyActionBar.BuildMenu(sv, cv, defaults)
             {
               type = "checkbox";
               name = "[EXPERIMENTAL] Show Tick Rate for Toggles";
-              tooltip = "Some toggled abilities have effects that `tick` while the ability is toggled, such as the resource return on Meditate. If enabled, the action bar will attempt to show the timer until the next tick. Load screens can cause this timer to desync from the game engine timer until the abilty is retoggled.";
+              tooltip = "Some toggled abilities have effects that `tick` while the ability is toggled, such as the resource return on Meditate. If enabled, the action bar will attempt to show the timer until the next tick. Load screens can cause this timer to desync from the game engine timer until the ability is retoggled.";
               default = defaults.showToggleTicks;
               getFunc = function() return SV.showToggleTicks; end;
               setFunc = function( value ) SV.showToggleTicks = value or false; end;
