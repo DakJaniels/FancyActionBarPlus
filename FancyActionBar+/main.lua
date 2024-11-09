f42fc3fbd27dfe27c1b92939646b4917201d00c6
Anthony S. Castanza <acastanza@gmail.com>
2024-11-09 11:51:40 -0800

    Bump Version

--- FancyActionBar+/main.lua
+++ FancyActionBar+/main.lua
@@ -4,7 +4,7 @@ local FancyActionBar = FancyActionBar;
 -----------------------------[    Constants   ]--------------------------------
 -------------------------------------------------------------------------------
 local NAME = "FancyActionBar+";
-local VERSION = "2.10.2";
+local VERSION = "2.10.3";
 local slashCommand = "/fab" or "/FAB";
 local EM = GetEventManager();
 local WM = GetWindowManager();
