5cb78e35b46f088ecb55ba956157626b3dfa0908
Anthony S. Castanza <acastanza@gmail.com>
2024-12-20 14:58:08 -0800

    Increment Version

--- FancyActionBar+/main.lua
+++ FancyActionBar+/main.lua
@@ -4,7 +4,7 @@ local FancyActionBar = FancyActionBar;
 -----------------------------[    Constants   ]--------------------------------
 -------------------------------------------------------------------------------
 local NAME = "FancyActionBar+";
-local VERSION = "2.10.12";
+local VERSION = "2.10.13";
 local slashCommand = "/fab" or "/FAB";
 local EM = GetEventManager();
 local WM = GetWindowManager();
